0144a3159e452daa61b3155167eca85eb9fc3a1e
cohena <cohena@gmail.com>
2015-01-01 17:52:48 -0800

    Enable LCD display

--- transit_display.py
+++ transit_display.py
@@ -3,7 +3,7 @@ from abbreviations import bart_abbreviations, city_abbreviations
 import nextbus
 import arrow
 from operator import itemgetter
-# import lcd
+import lcd
 
 LCD_WIDTH = 16
 LCD_HEIGHT = 2
@@ -73,12 +73,12 @@ def squish_text(text):
 
 
 def display_on_lcd(text_rows):
-    for row in text_rows[:LCD_HEIGHT]:
-        if len(row) > LCD_WIDTH:
-            print squish_text(row)[:LCD_WIDTH]
-        else:
-            print row.center(LCD_WIDTH)
-    # lcd.message("\n".join(text_rows[:LCD_HEIGHT]))
+    # for row in text_rows[:LCD_HEIGHT]:
+    #     if len(row) > LCD_WIDTH:
+    #         print squish_text(row)[:LCD_WIDTH]
+    #     else:
+    #         print row.center(LCD_WIDTH)
+    lcd.message("\n".join(text_rows[:LCD_HEIGHT]))
 
 
 def cycle_screens(dwell_time=5):
